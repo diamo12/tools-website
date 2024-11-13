73e6518dba0520f47d2f2f21015f2db5baf4ce27
Brandon Fields <superboomda@gmail.com>
2024-11-13 01:33:50 -0500

    Updated header component template, as well as all pages to reference the new component.

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,3 +1,98 @@
-<div>
-    <!-- Simplicity is the consequence of refined emotions. - Jean D'Alembert -->
-</div>
+<header class="rvt-header-wrapper">
+    
+    <!-- **********************************************************************
+        "Skip to main content" link for keyboard users
+    *********************************************************************** -->
+
+    <a class="rvt-header-wrapper__skip-link" href="#main-content">Skip to main content</a>
+    
+    <!-- **********************************************************************
+        Global header area
+    *********************************************************************** -->
+    
+    <div class="rvt-header-global">
+        <div class="rvt-container-lg">
+            <div class="rvt-header-global__inner">
+                <div class="rvt-header-global__logo-slot">
+                    <a class="rvt-lockup" href="#0">
+                        <div class="rvt-lockup__body">
+                            <span class="rvt-lockup__title">Diamo12's Web Tools &amp; Guides</span>
+                        </div>
+                    </a>
+                </div>
+                <div class="rvt-header-global__controls">
+                    <div data-rvt-disclosure="menu" data-rvt-close-click-outside>
+                        <button aria-expanded="false" class="rvt-global-toggle rvt-global-toggle--menu rvt-hide-lg-up" data-rvt-disclosure-toggle="menu">
+                            <span class="rvt-sr-only">Menu</span>
+                            <svg class="rvt-global-toggle__open" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">  <path d="M15 4H1V2h14v2Zm0 5H1V7h14v2ZM1 14h14v-2H1v2Z"/></svg>
+                            <svg class="rvt-global-toggle__close" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">  <path d="m3.5 2.086 4.5 4.5 4.5-4.5L13.914 3.5 9.414 8l4.5 4.5-1.414 1.414-4.5-4.5-4.5 4.5L2.086 12.5l4.5-4.5-4.5-4.5L3.5 2.086Z"/></svg>
+                        </button>
+
+                        <nav aria-label="Main" class="rvt-header-menu" data-rvt-disclosure-target="menu" hidden>
+                            <ul class="rvt-header-menu__list">
+                                <<li class="rvt-header-menu__item">
+                                    <div class="rvt-header-menu__dropdown rvt-dropdown" data-rvt-dropdown="primary-nav-1">
+                                        <div class="rvt-header-menu__group">
+                                            <!-- Link that appears in header -->
+                                            <a class="rvt-header-menu__link" href="">Tools</a>
+                                            <!-- Button that shows/hides dropdown links -->
+                                            <button aria-expanded="false" class="rvt-dropdown__toggle rvt-header-menu__toggle" data-rvt-dropdown-toggle="primary-nav-1">
+                                                <span class="rvt-sr-only">Tools sub-navigation links</span>
+                                                <svg class="rvt-global-toggle__open" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">  <path d="m15.146 6.263-1.292-1.526L8 9.69 2.146 4.737.854 6.263 8 12.31l7.146-6.047Z"/></svg>
+                                            </button>
+                                            </div>
+                                            <!-- Dropdown menu -->
+                                            <div class="rvt-header-menu__submenu rvt-dropdown__menu rvt-dropdown__menu--right" data-rvt-dropdown-menu="primary-nav-1" hidden>
+                                            <ul class="rvt-header-menu__submenu-list">
+                                                <!-- Dropdown links -->
+                                                <li class="rvt-header-menu__submenu-item">
+                                                <a class="rvt-header-menu__submenu-link" href="">Image Cropper Tool</a>
+                                                </li>
+                                                <li class="rvt-header-menu__submenu-item">
+                                                <a class="rvt-header-menu__submenu-link" href="">Tool 2</a>
+                                                </li>
+                                                <li class="rvt-header-menu__submenu-item">
+                                                <a class="rvt-header-menu__submenu-link" href="">Tool 3</a>
+                                                </li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                </li>
+
+                                <<li class="rvt-header-menu__item">
+                                    <div class="rvt-header-menu__dropdown rvt-dropdown" data-rvt-dropdown="primary-nav-1">
+                                        <div class="rvt-header-menu__group">
+                                            <!-- Link that appears in header -->
+                                            <a class="rvt-header-menu__link" href="">Guides</a>
+                                            <!-- Button that shows/hides dropdown links -->
+                                            <button aria-expanded="false" class="rvt-dropdown__toggle rvt-header-menu__toggle" data-rvt-dropdown-toggle="primary-nav-1">
+                                                <span class="rvt-sr-only">Guides sub-navigation links</span>
+                                                <svg class="rvt-global-toggle__open" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">  <path d="m15.146 6.263-1.292-1.526L8 9.69 2.146 4.737.854 6.263 8 12.31l7.146-6.047Z"/></svg>
+                                            </button>
+                                            </div>
+                                            <!-- Dropdown menu -->
+                                            <div class="rvt-header-menu__submenu rvt-dropdown__menu rvt-dropdown__menu--right" data-rvt-dropdown-menu="primary-nav-1" hidden>
+                                            <ul class="rvt-header-menu__submenu-list">
+                                                <!-- Dropdown links -->
+                                                <li class="rvt-header-menu__submenu-item">
+                                                <a class="rvt-header-menu__submenu-link" href="">Guide 1</a>
+                                                </li>
+                                                <li class="rvt-header-menu__submenu-item">
+                                                <a class="rvt-header-menu__submenu-link" href="">Guide 2</a>
+                                                </li>
+                                                <li class="rvt-header-menu__submenu-item">
+                                                <a class="rvt-header-menu__submenu-link" href="">Guide 3</a>
+                                                </li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                </li>
+                            </ul>
+
+                        </nav>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -9,50 +9,7 @@
         <link rel="stylesheet" href="https://unpkg.com/rivet-core@2.8.1/css/rivet.min.css">
     </head>
     <body>
-        <header class="rvt-header-wrapper">
-    
-            <!-- **********************************************************************
-                "Skip to main content" link for keyboard users
-            *********************************************************************** -->
-
-            <a class="rvt-header-wrapper__skip-link" href="#main-content">Skip to main content</a>
-            
-            <!-- **********************************************************************
-                Global header area
-            *********************************************************************** -->
-            
-            <div class="rvt-header-global">
-                <div class="rvt-container-lg">
-                    <div class="rvt-header-global__inner">
-                        <div class="rvt-header-global__logo-slot">
-                            <a class="rvt-lockup" href="#0">
-                                <div class="rvt-lockup__body">
-                                    <span class="rvt-lockup__title">Diamo12's Web Tools &amp; Guides</span>
-                                </div>
-                            </a>
-                        </div>
-                        <div class="rvt-header-global__controls">
-                            <div data-rvt-disclosure="menu" data-rvt-close-click-outside>
-                                <button aria-expanded="false" class="rvt-global-toggle rvt-global-toggle--menu rvt-hide-lg-up" data-rvt-disclosure-toggle="menu">
-                                    <span class="rvt-sr-only">Menu</span>
-                                    <svg class="rvt-global-toggle__open" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">  <path d="M15 4H1V2h14v2Zm0 5H1V7h14v2ZM1 14h14v-2H1v2Z"/></svg>
-                                    <svg class="rvt-global-toggle__close" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">  <path d="m3.5 2.086 4.5 4.5 4.5-4.5L13.914 3.5 9.414 8l4.5 4.5-1.414 1.414-4.5-4.5-4.5 4.5L2.086 12.5l4.5-4.5-4.5-4.5L3.5 2.086Z"/></svg>
-                                </button>
-
-                                <nav aria-label="Main" class="rvt-header-menu" data-rvt-disclosure-target="menu" hidden>
-                                    <ul class="rvt-header-menu__list">
-                                        <li class="rvt-header-menu__item">
-                                            <a class="rvt-header-menu__link" href="">Image Cropper Tool</a>
-                                        </li>
-                                    </ul>
-
-                                </nav>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </header>
+        <x-header/>
         <main id="main-content" class="rvt-flex rvt-flex-column rvt-grow-1">
             <div class="rvt-billboard">
                 <div class="rvt-billboard__body">
