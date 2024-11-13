795c80dbdb8f67f24cdc190ad09fca9bf0419dd2
Brandon Fields <superboomda@gmail.com>
2024-11-13 01:04:05 -0500

    Updating the site.

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,9 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
+});
+
+Route::get('/decoy-1', function () {
+    return view('secret');
 });
